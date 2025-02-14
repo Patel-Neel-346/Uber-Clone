2e41eaed00df4fa5c7c2bb72c5a514e79e755255
Patel-Neel-346 <neelpatel6340@gmail.com>
2025-02-14 22:20:09 +0530

    commit

--- frontend/src/pages/Main.jsx
+++ frontend/src/pages/Main.jsx
@@ -20,7 +20,7 @@ const Main = () => {
     }
   },[panelOpen])
   return (
-    <div className='h-screen relative'>
+    <div className='h-screen relative overflow-hidden'>
       <img className=' w-16 absolute left-5 top-5  ' src="https://upload.wikimedia.org/wikipedia/commons/c/cc/Uber_logo_2018.png" alt="" />
 
       <div className=' h-screen w-screen'>
@@ -40,6 +40,36 @@ const Main = () => {
         <div ref={panelRef} className='h-0 bg-white '>
             <LocationSearchPanel/>
 
+        </div>
+      </div>
+      <div className=' fixed w-full z-10 bottom-0 bg-white p-3 '>
+        <h3 className=' text-2xl font-semibold mb-3 '>Choose a Vehicle</h3>
+        <div className=' mb-3 flex active:border-2 border-black rounded-xl w-full p-3  items-center justify-between '>
+          <img className=' h-12 ' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1568134115/assets/6d/354919-18b0-45d0-a151-501ab4c4b114/original/XL.png" alt="" />
+          <div className='w-1/2'>
+            <h4 className=' font-medium text-sm'>UberGO <span><i className="ri-user-fill"></i>4</span></h4>
+            <h5 className=' font-medium text-xs'>2 mins away</h5>
+            <p className=' font-xl text-xs text-gray-600'>Affordable,compact rides</p>
+          </div>
+          <h2 className=' text-2xl font-semibold'>$20</h2>
+        </div>
+        <div className='mb-3 flex active:border-2 border-black rounded-xl w-full p-3  items-center justify-between '>
+          <img className=' h-12 ' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648177797/assets/fc/ddecaa-2eee-48fe-87f0-614aa7cee7d3/original/Uber_Moto_312x208_pixels_Mobile.png" alt="" />
+          <div className='w-1/2'>
+            <h4 className=' font-medium text-sm'>UberGO <span><i className="ri-user-fill"></i>4</span></h4>
+            <h5 className=' font-medium text-xs'>2 mins away</h5>
+            <p className=' font-xl text-xs text-gray-600'>Affordable,compact rides</p>
+          </div>
+          <h2 className=' text-2xl font-semibold'>$20</h2>
+        </div>
+        <div className='mb-3 flex active:border-2 border-black rounded-xl w-full p-3  items-center justify-between '>
+          <img className=' h-12 ' src="https://www.uber-assets.com/image/upload/f_auto,q_auto:eco,c_fill,h_368,w_552/v1648431773/assets/1d/db8c56-0204-4ce4-81ce-56a11a07fe98/original/Uber_Auto_558x372_pixels_Desktop.png" alt="" />
+          <div className='w-1/2'>
+            <h4 className=' font-medium text-sm'>UberGO <span><i className="ri-user-fill"></i>4</span></h4>
+            <h5 className=' font-medium text-xs'>2 mins away</h5>
+            <p className=' font-xl text-xs text-gray-600'>Affordable,compact rides</p>
+          </div>
+          <h2 className=' text-2xl font-semibold'>$20</h2>
         </div>
       </div>
     </div>
