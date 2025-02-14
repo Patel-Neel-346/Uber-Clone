7e961fb902024f0c31645fc0d88823657a481f38
Patel-Neel-346 <neelpatel6340@gmail.com>
2025-02-14 22:33:18 +0530

    commit

--- frontend/src/components/LocationSearchPanel.jsx
+++ frontend/src/components/LocationSearchPanel.jsx
@@ -1,37 +1,25 @@
 import React from 'react'
 
 const LocationSearchPanel = () => {
+  //sample location
+  const locations = [
+    "24A,Near Kapor's cafe,Sheryins coding school,Bhopal",
+    "24B,Near Kapor's cafe,Sheryins coding school,Bhopal",
+    "24C,Near Kapor's cafe,Sheryins coding school,Bhopal",
+  ]
   return (
     <div>
+      {
+        locations.map(function (e) {
+          return <div className='active:border-2 cursor-pointer flex gap-4 p-4 items-center justify-start'>
+            <h2 className=' bg-[#eee] h-10 w-12 flex items-center justify-center rounded-full '><i className="ri-map-pin-fill"></i></h2>
+            <h4>{e}</h4>
+          </div>
+        })
+      }
       {/* this is just sample data */}
-      <div className=' cursor-pointer flex gap-4 p-4 items-center justify-start'>
-        <h2 className=' bg-[#eee] h-10 w-12 flex items-center justify-center rounded-full '><i className="ri-map-pin-fill"></i></h2>
-        <h4>24B,Near Kapor's cafe,Sheryins coding school,Bhopal</h4>
-      </div>
-      <div className=' cursor-pointer flex gap-4 p-4 items-center justify-start'>
-        <h2 className=' bg-[#eee] h-10 w-12 flex items-center justify-center rounded-full '><i className="ri-map-pin-fill"></i></h2>
-        <h4>24B,Near Kapor's cafe,Sheryins coding school,Bhopal</h4>
-      </div>
-      <div className=' cursor-pointer flex gap-4 p-4 items-center justify-start'>
-        <h2 className=' bg-[#eee] h-10 w-12 flex items-center justify-center rounded-full '><i className="ri-map-pin-fill"></i></h2>
-        <h4>24B,Near Kapor's cafe,Sheryins coding school,Bhopal</h4>
-      </div>
-      <div className=' cursor-pointer flex gap-4 p-4 items-center justify-start'>
-        <h2 className=' bg-[#eee] h-10 w-12 flex items-center justify-center rounded-full '><i className="ri-map-pin-fill"></i></h2>
-        <h4>24B,Near Kapor's cafe,Sheryins coding school,Bhopal</h4>
-      </div>
-      <div className=' cursor-pointer flex gap-4 p-4 items-center justify-start'>
-        <h2 className=' bg-[#eee] h-10 w-12 flex items-center justify-center rounded-full '><i className="ri-map-pin-fill"></i></h2>
-        <h4>24B,Near Kapor's cafe,Sheryins coding school,Bhopal</h4>
-      </div>
-      <div className=' cursor-pointer flex gap-4 p-4 items-center justify-start'>
-        <h2 className=' bg-[#eee] h-10 w-12 flex items-center justify-center rounded-full '><i className="ri-map-pin-fill"></i></h2>
-        <h4>24B,Near Kapor's cafe,Sheryins coding school,Bhopal</h4>
-      </div>
-      <div className=' cursor-pointer flex gap-4 p-4 items-center justify-start'>
-        <h2 className=' bg-[#eee] h-10 w-12 flex items-center justify-center rounded-full '><i className="ri-map-pin-fill"></i></h2>
-        <h4>24B,Near Kapor's cafe,Sheryins coding school,Bhopal</h4>
-      </div>
+
+
     </div>
   )
 }
