905cc84f3be342b85bda9a7ce2e520dddbd8a04e
Patel-Neel-346 <neelpatel6340@gmail.com>
2025-03-03 21:55:44 +0530

    Socket Io at Backend

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,10 @@
 const http=require('http');
 const app=require('./app');
+const { initialzeSocket } = require('./socket');
 const Port=process.env.PORT || 5000;
 const server=http.createServer(app);
 
+initialzeSocket(server)
 server.listen(Port,()=>{
     console.log(`Server is running on port ${Port}`)
 })
---
+++ backend/socket.js
@@ -0,0 +1,44 @@
+const socketIo=require('socket.io');
+const User = require('./models/user_model');
+const captain_model = require('./models/captain_model');
+
+let io;
+
+function initialzeSocket(server){
+    io=socketIo(server,{
+        cors:{
+            origin:'*',
+            methods:['GET','POST']
+        }
+    });
+
+    io.on('connection',(socket)=>{
+        console.log("Client Connected :",socket.id);
+
+        
+        socket.on('join', async (data) => {
+            const { userId, userType } = data;
+
+            if (userType === 'user') {
+                await User.findByIdAndUpdate(userId, { socketId: socket.id });
+            } else if (userType === 'captain') {
+                await captain_model.findByIdAndUpdate(userId, { socketId: socket.id });
+            }
+        });
+
+
+        socket.on('disconnect',()=>{
+            console.log(`Client is Disconnected:${socket.id}`);
+        });
+    });
+}
+
+function sendMessageToSocketId(socketId,message){
+    if(io){
+        io.to(socketId).emit('message',message);
+    }else{
+        console.log('Socket.io is not Initalized');
+    }
+}
+
+module.exports={initialzeSocket,sendMessageToSocketId}
